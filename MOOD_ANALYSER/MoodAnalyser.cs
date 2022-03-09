652adad7a90427dd7edfa15cdcda466a0b1590dd
Shridhartadakod <shridhar1491998@gmail.com>
2022-03-09 17:07:26 +0530

    Analyse mood

---
+++ MOOD_ANALYSER/MoodAnalyser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOOD_ANALYSER
+{
+    public class MoodAnalyser
+    {
+        string message;
+
+        public MoodAnalyser()
+        {
+            message = "";
+        }
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        /// <summary>
+        /// Analyses the mood.
+        /// </summary>
+        public string AnalyseMood()
+        {
+            if (message.Contains("Sad", StringComparison.OrdinalIgnoreCase) is true)
+                return "Sad";
+            return "Happy";
+        }
+
+
+    }
+}
--- MOOD_ANALYSER/Program.cs
+++ MOOD_ANALYSER/Program.cs
@@ -11,6 +11,12 @@ namespace MOOD_ANALYSER
         static void Main(string[] args)
         {
             Console.WriteLine("mood analyser program:");
+            MoodAnalyser mood = new MoodAnalyser("I am really sad");
+
+            string moodStatus = mood.AnalyseMood();
+
+            Console.WriteLine("Input Text: I am really sad");
+            Console.WriteLine("Mood: " + moodStatus);
 
 
         }
